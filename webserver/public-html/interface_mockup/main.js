f6a19a1f1787f6fc2b055ccebe7788c17fae046c
Unknown <zackrisongeorge@gmail.com>
2016-09-30 17:30:41 -0600

    Updated GUI js and html

--- webserver/public-html/interface_mockup/main.js
+++ webserver/public-html/interface_mockup/main.js
@@ -93,15 +93,3 @@ $("#mid").html(
 	"<button type='button' class='flex-mid'><i class='material-icons'>stop</i></button>" +
 	"<button type='button' class='flex-mid'><i class='material-icons'>redo</i></button>"
 );
-
-var d = new Date();
-
-$("#footer").html(
-	"<div class='footer'>" +
-	"<div class='footer-label'><p>Contact</p></div>" +
-	"<a href='mailto:zackrisongeorge@gmail.com'><img id='special' class='footer-icon' src='../icons/email.png' alt='Gmail'></a>" +
-	"<a href='https://plus.google.com/u/0/+GeorgeZackrison'target='_blank'><img id='special' class='footer-icon' src='../icons/plus.png' alt='G+'></a>" +
-	"<a href='https://twitter.com/ZackrisonGeorge' target='_blank'><img id='special' class='footer-icon' src='../icons/twitter.png' alt='Twitter'></a>" +
-	"<div class='copyright'><p class='copyright-icon'><i class='material-icons'>copyright</i> " + (d.getFullYear()) + " George Zackrison</p><p class='copyright-label'>All rights reserved.</p></div>" +
-	"</div>"
-);
